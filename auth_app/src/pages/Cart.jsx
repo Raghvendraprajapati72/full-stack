39d3f171d5ca2984c22f6a2a0189e3ba4f87b8f4
RaghvendraPRAJAPATI72 <raghvendraprjapati72@gmail.com>
2026-05-10 23:44:51 +0530

    fixed cart api urls

--- auth_app/src/pages/Cart.jsx
+++ auth_app/src/pages/Cart.jsx
@@ -13,7 +13,7 @@ export default function Cart() {
     if (!user) return;
 
     axios
-      .get(`http://https://full-stack-backend-qps4.onrender.com:5000/cart/${user.id}`)
+      .get(`https://full-stack-backend-qps4.onrender.com/cart/${user.id}`)
       .then((res) => {
         console.log("CART:", res.data);
 
@@ -33,7 +33,7 @@ export default function Cart() {
   const removeItem = async (id) => {
     try {
       await axios.delete(
-        `http://https://full-stack-backend-qps4.onrender.com:5000/cart/remove/${id}`
+        `https://full-stack-backend-qps4.onrender.com/cart/remove/${id}`
       );
 
       setItems(items.filter((i) => i.id !== id));
@@ -83,7 +83,7 @@ export default function Cart() {
                     src={
                       item.image?.startsWith("http")
                         ? item.image
-                        : `http://https://full-stack-backend-qps4.onrender.com:5000${item.image}`
+                        : `https://full-stack-backend-qps4.onrender.com${item.image}`
                     }
                     alt={item.name}
                     style={img}
